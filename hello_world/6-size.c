7c55033134fd87eeb8b3d46265002b8eecbe957a
Washx23 <5731@holbertonstudents.com>
2022-09-27 11:37:01 -0700

    44

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	char charType;
 	float floatType;
 
-	printf("Size of a char: 1 bytes\n", sizeof(charType));
-	printf("Size of an int: 4 bytes\n", sizeof(intType));
-	printf("Size of long int: 8 bytes\n", sizeof(longintType));
-	printf("Size of long long int: 8 byte\n", sizeof(longlongType));
-	printf("Size of float: 4 byte\n", sizeof(floatType));
+	printf("Size of a char: 1 byte(s)\n", sizeof(charType));
+	printf("Size of an int: 4 byte(s)\n", sizeof(intType));
+	printf("Size of long int: 4 byte(s)\n", sizeof(longintType));
+	printf("Size of long long int: 8 byte(s)\n", sizeof(longlongType));
+	printf("Size of float: 4 byte(s)\n", sizeof(floatType));
 	return (0);
 }
