c14435a2abb006599dcb09a5c0d31d9b44e1203e
Washx23 <5731@holbertonstudents.com>
2022-09-27 10:04:53 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-		/* Este es mi primer programa en C*/
-
-		printf("Bienvenido a la programación en lenguaje C\n");
-
-			return 0;
-
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
+
