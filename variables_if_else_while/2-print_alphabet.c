3cc31135c68ce89424731e9818fdcf83b0a9dc94
Washx23 <5731@holbertonstudents.com>
2022-09-28 10:15:33 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - assign
+ * alphabet
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
