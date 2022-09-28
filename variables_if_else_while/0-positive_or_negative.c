f3ae5f80501aed538a6e0cd9f2c49a569b4e7668
Washx23 <5731@holbertonstudents.com>
2022-09-28 07:44:29 -0700

    0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry points
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
