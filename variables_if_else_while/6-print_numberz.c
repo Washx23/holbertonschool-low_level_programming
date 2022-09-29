219d74e4f59ffbc1243323b41294b8d6527c254a
Washx23 <5731@holbertonstudents.com>
2022-09-29 07:01:34 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - numbers
+ * :(
+ * Return: Always(0) (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
