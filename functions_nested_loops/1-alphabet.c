03aae3dc57cc8df6806fb539369344df0d93f28e
Washx23 <5731@holbertonstudents.com>
2022-09-30 11:17:59 -0700

    1-alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{ 
+	int q='0';
+	_putchar(q);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,14 @@
 #include <unistd.h>
 
-/**
- * _putchair - writes the character c to stdout
- * @c: The character to print 
- *
- * Return
- *
- *
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
