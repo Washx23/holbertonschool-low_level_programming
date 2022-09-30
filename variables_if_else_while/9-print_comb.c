349a2f6f2123f2b4d4c2c184e4f86aa4f30b56c3
Washx23 <5731@holbertonstudents.com>
2022-09-30 06:44:37 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - assign
+ * tebahpla
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+	}
+}
+
