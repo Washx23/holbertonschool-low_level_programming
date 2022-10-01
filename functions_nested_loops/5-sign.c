09108de62f7053c2761d3dea9736358028eb6a2b
Washx23 <5731@holbertonstudents.com>
2022-10-01 15:19:26 -0700

    5-sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - print
+ * @n: is a number
+ * Return: success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+}
