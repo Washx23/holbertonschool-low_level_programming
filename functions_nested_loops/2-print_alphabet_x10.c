40171b918cc4df54aca88b752b5af6239794762d
Washx23 <5731@holbertonstudents.com>
2022-10-01 12:06:45 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,17 @@ void print_alphabet_x10(void)
 	char ch;
 	int i;
 
-	for (i = 1; i <= 10; i++)
+	i = 0;
+
+	while (i < 10)
 	{
-	for (ch = 'a'; ch <= 'z'; ch++)
+		ch = 'a';
+		while (ch <= 'z')
 		{
-		_putchar(ch);
+			_putchar(ch);
+			ch++;
 		}
+		_putchar('\n');
+		i++;
 	}
-	_putchar('\n');
 }
