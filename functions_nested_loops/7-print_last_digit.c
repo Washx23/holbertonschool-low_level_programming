512bc83d141df3eaebf58a86681ef944820b0f4f
Washx23 <5731@holbertonstudents.com>
2022-10-03 07:13:09 -0700

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,14 @@ int print_last_digit(int num)
 {
 	int digit;
 
+	if (num < 0)
+		num = -num;
 	digit = num % 10;
+
+	if (digit < 0)
+		digit = -digit;
+
+	_putchar(digit + '0');
+
 	return (digit);
 }
