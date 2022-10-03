4feb19df3382b340c4b13a9c12f5a41c3b5b0aa4
Washx23 <5731@holbertonstudents.com>
2022-10-03 09:38:52 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds character
+ * @a: character 1
+ * @b: character 2
+ * Return: character
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
