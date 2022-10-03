fa800dfa4c2b9632e9dacd7b0d4fde6ff367eb77
Washx23 <5731@holbertonstudents.com>
2022-10-03 07:01:55 -0700

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - funcion
+ * @num: number use
+ * Return: digit
+ */
+
+int print_last_digit(int num)
+{
+	int digit;
+
+	digit = num % 10;
+	return (digit);
+}
