ce5af1f27ac5d37b63a3fe679f2d5822376babd2
Washx23 <5731@holbertonstudents.com>
2022-10-05 11:10:52 -0700

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				if (y != size && y <= size - x)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(35);
+				}
+			}
+			_putchar('\n');
+
+		}
+	}
+	else
+		_putchar('\n');
+}
