aa085d0970da8cbacfe84a1cc2fe711ca6e9406f
Washx23 <5731@holbertonstudents.com>
2022-10-05 10:29:48 -0700

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,20 +9,24 @@ int main(void)
 
 	for ( i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 15 == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz");
 		}
-		if (i % 5 == 0)
+		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
-		if ((i % 3 == 0) && (i % 5 == 0))
+		else if (i % 3 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("Fizz");
 		}
 		else
-			printf("%i ", i);
+			printf("%i", i);
+		if (i < 100)
+		{
+			printf(" ");
+		}
 	}
 	return (0);
 }
