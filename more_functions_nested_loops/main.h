bec30fc6534a2fafa65d40fa1c75959b868695bd
Washx23 <5731@holbertonstudents.com>
2022-10-05 10:21:27 -0700

    9-fizz_buzz

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+#include <stdio.h>
 int _putchar(int c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,4 +11,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
