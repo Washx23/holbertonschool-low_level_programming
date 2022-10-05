7952e8ea1ee3b4deb74a0822cf35c0c9f1c11901
Washx23 <5731@holbertonstudents.com>
2022-10-05 05:21:39 -0700

    4-print_most_numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,14 @@
  */
 void print_most_numbers(void)
 {
-	int n = '0';
+	int n;
 
-	while (n <= '9')
+	for (n = '0'; n <= '9'; n++)
 	{
 		if (n != '2' && n != '4')
 		{
 			putchar(n);
 		}
-		n++;
 	}
 	putchar('\n');
-	return (0);
 }
