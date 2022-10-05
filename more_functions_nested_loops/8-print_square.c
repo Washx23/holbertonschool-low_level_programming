4fe0e717a90b2cae883e5446ca21c6b298df2962
Washx23 <5731@holbertonstudents.com>
2022-10-05 09:36:23 -0700

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - funcion
+ * Return: result
+ * @n: variable
+ */
+void print_square(int size)
+{
+
+	int x, y;
+
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1 ; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar(35);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
