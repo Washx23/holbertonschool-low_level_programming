ce8a08fe8fc7c672dfc1e884dbbcc022fa6ec079
Washx23 <5731@holbertonstudents.com>
2022-10-05 08:30:37 -0700

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,9 +12,9 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (x = 0; x <= n; x++)
+		for (x = 0; x < n; x++)
 		{
-			for (y = 0 ; y < x; y++)
+			for (y = 0 ; y <= x; y++)
 			{
 				_putchar(' ');	
 			}
