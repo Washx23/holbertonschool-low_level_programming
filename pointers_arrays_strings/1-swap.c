ec3c00e0031cf4b4db8dbaab00c93742b7883b61
Washx23 <5731@holbertonstudents.com>
2022-10-06 09:01:02 -0700

    1-swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - funcion
+ * @a: variable
+ * @b: variable
+ * @n: variable
+ * Return: always
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	*a = *b;
+	*b = n;
+}
+
+
