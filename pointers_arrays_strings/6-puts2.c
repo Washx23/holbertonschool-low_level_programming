9ad879d9c8480f0eaef666b7d475a453be8ebec4
Washx23 <5731@holbertonstudents.com>
2022-10-11 07:27:58 -0700

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,25 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int cont = _strlen(str);
 
-	if ( i % 2 == 0)
+	for (; i < cont; i += 2)
 	{
-		while (*str)
-		{
-			_putchar(*str);
-			str = str + 2;
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
+}
+/**
+ * _strlen - fucion
+ * @s: variable
+ * Return: always
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
 }
