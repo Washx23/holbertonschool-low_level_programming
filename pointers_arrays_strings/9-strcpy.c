75db5090d6d55ccfc9dca5e1b2baf0552425ffb1
Washx23 <5731@holbertonstudents.com>
2022-10-12 08:21:33 -0700

    9-strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _strcpy - funcion
+ * *_strcpy - funcion
  * @src: pointer
  * @dest: pointer
- *
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' + 1 ; i++)
+	for (i = 0; src[i] != '\0' ; i++)
 	{
-		dest[i] = src[i];
+		dest[i] = src[i + '\0'];
 	}
 		return (dest);
 }
