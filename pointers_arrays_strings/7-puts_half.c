e87701d4294a908c3e193d78ccde8dcf5d171c83
Washx23 <5731@holbertonstudents.com>
2022-10-12 06:06:45 -0700

    7-puts_half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void puts_half(char *str)
+{
+	int c = 0;
+	int len = 0;
+
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+	len = (c) / 2;
+	
+	while (len < c && len != c)
+	{
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}
