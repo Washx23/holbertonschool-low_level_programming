c6023cdfa6b4401f5ea5abb02d827c50476d6e4d
Washx23 <5731@holbertonstudents.com>
2022-10-12 08:27:44 -0700

    9-strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -11,8 +11,9 @@ char *_strcpy(char *dest, char *src)
 
 	for (i = 0; src[i] != '\0' ; i++)
 	{
-		dest[i] = src[i + '\0'];
+		dest[i] = src[i];
 	}
-		return (dest);
+	dest[i] = '\0';
+	return (dest);
 }
 
