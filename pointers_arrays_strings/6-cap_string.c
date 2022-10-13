e36e991ae3e1ba43faf5be1657d8f57d56856434
Washx23 <5731@holbertonstudents.com>
2022-10-13 10:26:11 -0700

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * cap_string - funcion
+ * @i: pointer
+ * Return: always
+ *
+ */
+char *cap_string(char *i)
+{
+	int p;
+
+	for (p = 0; i[p] != '\0'; p++)
+	{
+		if (i == 0)
+		{
+			if (i[p] >= 97 && i[p] <= 122)
+			{
+				i[p] = i[p] - 32;
+			}
+		}
+		else if (i[p - 1] >= 32 && i[p - 1] <= 47)
+		{
+			if (i[p] >= 97 && i[p] <= 122)
+			{
+				i[p] = i[p] - 32;
+			}
+		}
+		else if (i[p - 1] >= 48 && i[p - 1] <= 57)
+		{
+			if (i[p] >= 97 && i[p] <= 122)
+			{
+				i[p] = i[p] - 32;
+			}
+		}
+		else if (i[p - 1] >= 9 && i[p - 1] <= 10)
+		{
+			if (i[p] >= 97 && i[p] <= 122)
+			{
+				i[p] = i[p] - 32;
+			}
+		}
+	}
+	return (i);
+}
