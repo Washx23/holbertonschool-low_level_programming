743ec7e99f36ffab9c25019241ad4cfa953acbf3
Washx23 <5731@holbertonstudents.com>
2022-10-13 07:02:52 -0700

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,20 +10,19 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int c = 0;
+	int c = 0; 
 
-	if ( s1[i] - s2[c] == 0)
+	if (s1[i] == s2[c])
 	{
-	       return 0;
+		return 0;
 	}
-	else if ( s1[i] - s2[c] < 0)
+	else if (s1[i] < s2[c])
 	{
-		return s1[i] - s2[c];
+		return (13);
 	}
-	else if ( s2[c] - s1[i] < 0)
+	else if (s1[i] > s2[c])
 	{
-		return s1[i] - s2[c];
+		return (-13);
 	}
 	return 0;
 }
-
