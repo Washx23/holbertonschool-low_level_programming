5a3d2df2b608a6cfa575c1280c0082fe5aa38b50
Washx23 <5731@holbertonstudents.com>
2022-10-13 07:43:26 -0700

    4-rev_array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * reverse_array - funcion
+ * @a : pointer
+ * @n: is a variable
+ */
+void reverse_array(int *a, int n)
+{
+	int c;
+	int i;
+	int j;
+
+	while (a[n] < n)
+	{
+		n++;
+	}
+
+	c = n;
+
+	for (i = 0, j = c - 1; i < j; i++, j--)
+	{
+		char ch = i;
+
+		a[i] = a[j];
+		a[j] = ch;
+	}
+}
