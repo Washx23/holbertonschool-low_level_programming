960d9029c014935fbef8d8a799d5ed33451d7e66
Washx23 <5731@holbertonstudents.com>
2022-10-13 07:03:43 -0700

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -18,11 +18,11 @@ int _strcmp(char *s1, char *s2)
 	}
 	else if (s1[i] < s2[c])
 	{
-		return (13);
+		return (-13);
 	}
 	else if (s1[i] > s2[c])
 	{
-		return (-13);
+		return (13);
 	}
 	return 0;
 }
