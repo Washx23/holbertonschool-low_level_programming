a6367141f9acdbdcfb336f422231db041537ff31
Washx23 <5731@holbertonstudents.com>
2022-10-13 15:01:41 -0700

    8-print_diagsums.c

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagsums - function
+ * @a: pointer
+ * @size: ii
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int first_diag = 1;
+	int second_diag = size;
+	int num1 = a[0];
+	int num2 = a[size - 1];
+
+	for (i = 0; i < size * size; i++)
+	{
+		if (i != 0 && i % size == 0)
+		{
+			first_diag = first_diag + size;
+			second_diag = second_diag + size;
+			first_diag++;
+			second_diag--;
+			num1 += a[first_diag - 1];
+			num2 += a[second_diag - 1];
+		}
+	}
+	printf("%d, %d\n", num1, num2);
+}
