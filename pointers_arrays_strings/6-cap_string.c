629aba095b26aae7b286efbc7d4bb12255dd2a6c
Washx23 <5731@holbertonstudents.com>
2022-10-13 10:35:38 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,7 @@ char *cap_string(char *i)
 				i[p] = i[p] - 32;
 			}
 		}
-		else if (i[p - 1] >= 32 && i[p - 1] <= 44)
+		else if (i[p - 1] >= 32 && i[p - 1] <= 47 && i[p - 1] != 45)
 		{
 			if (i[p] >= 97 && i[p] <= 122)
 			{
