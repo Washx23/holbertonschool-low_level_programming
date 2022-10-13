ec131f0fb1fb8cb1cddf940170e4d3e7dc135dae
Washx23 <5731@holbertonstudents.com>
2022-10-13 07:15:09 -0700

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,28 +1,21 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
- *
+ * *_strcmp - the function strcmp
+ *@s1: pointer
+ *@s2: pointer
+ *Return: result
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int c = 0; 
 
-	if (s1[i] == s2[c])
+	for (; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		return 0;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-	else if (s1[i] < s2[c])
-	{
-		return (s1[i] - s2[c]);
-	}
-	else if (s1[i] > s2[c])
-	{
-		return (s2[c] - s1[i]);
-	}
-	return 0;
+	return (0);
 }
