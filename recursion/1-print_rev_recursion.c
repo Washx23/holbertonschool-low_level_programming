5db3432bed91ee47d5c814bace3229f4f35bdf7c
Washx23 <5731@holbertonstudents.com>
2022-10-17 06:33:19 -0700

    1-print_rev_recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - funcion
+ * @s: pointer
+ *
+ *
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		 _print_rev_recursion(s + 1);
+		 _putchar(*s);
+	}
+}	
+
