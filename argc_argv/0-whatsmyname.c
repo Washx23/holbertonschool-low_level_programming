0b5a76b615ac16f5c718dae5ed1dc3185645eba9
Washx23 <5731@holbertonstudents.com>
2022-10-18 06:55:10 -0700

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- *
- *
+ *main - funcion
+ * @argc: number argument
+ * @argv: argument
+ * Return: always
  */
-int main( int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i = 0; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
