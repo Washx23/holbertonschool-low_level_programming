81e09f8a060acc555cdbabc7522b7875c4596ab7
Washx23 <5731@holbertonstudents.com>
2022-10-24 04:56:37 -0700

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+char *_strcpy(char *dest, char *src);
+/**
+ * _strdup - funcion
+ * @str: pointer
+ * Return: always
+ */
+char *_strdup(char *str)
+{
+	char *a = NULL;
+
+	a = malloc(sizeof(str));
+	if (a == NULL)
+		return (NULL);
+	a = _strcpy(a, str);
+	return (a);
+}
+
+/**
+ * *_strcpy - funcion
+ * @src: pointer
+ * @dest: pointer
+ * Return: always
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
