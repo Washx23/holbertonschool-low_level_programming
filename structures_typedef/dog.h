6e0ff73f607b439f706e6b987a57da2c728e274d
Washx23 <5731@holbertonstudents.com>
2022-10-28 08:10:43 -0700

    task

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,17 @@
+#ifndef dog_h
+#define dog_h
 #include <stdio.h>
-
+/**
+ * struct dog - my first structure
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: structure of dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
