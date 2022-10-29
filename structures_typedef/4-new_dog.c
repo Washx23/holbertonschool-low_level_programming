20fa641e11d4947737c391c5255bccfe1dd5f7fc
Washx23 <5731@holbertonstudents.com>
2022-10-29 15:04:52 -0700

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -32,7 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		modok->owner = strdup(owner);
 		if (modok->owner == NULL)
 		{
-			free(name)
+			free(name);
 			free(modok);
 			return (NULL);
 		}
