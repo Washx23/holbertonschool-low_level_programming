9dd3c8ec3b8f320a5bd77bf021df9fa5d9aebe0e
Washx23 <5731@holbertonstudents.com>
2022-10-29 13:52:32 -0700

    task 4 bien hecho con corchetes

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ *new_dog - creates modok the destructor
+ *@name: the name of the dog
+ *@age: the age of the dog
+ *@owner: the owner of the dog/s
+ *Return: modok the destructor
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *modok = NULL;
+
+	modok = malloc(sizeof(dog_t));
+
+	if (modok != NULL)
+	{
+		modok->name = strdup(name);
+
+		if (modok->name == NULL)
+		{
+			free(modok);
+			return (NULL);
+		}
+		modok->owner = strdup(owner);
+		if (modok->owner == NULL)
+		{
+			free(modok);
+			return (NULL);
+		}
+		modok->age = age;
+	}
+	return (modok);
+}
