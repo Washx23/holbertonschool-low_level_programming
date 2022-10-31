8318bedb8290345fafda515d3d9fdaecabb78b8e
Washx23 <5731@holbertonstudents.com>
2022-10-31 12:12:58 -0700

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,42 +1,82 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <stdlib.h>
 
+char *_strdup(char *str);
 /**
- *new_dog - creates modok the destructor
- *@name: the name of the dog
- *@age: the age of the dog
- *@owner: the owner of the dog/s
- *Return: modok the destructor
+ * new_dog - creates a new dog.
+ * @name: string of char
+ * @age: float
+ * @owner: string of char
+ * Return: a pointer to the struct dog or NULL if the function fails
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *modok = NULL;
+	dog_t *new_d = NULL;
 
-	modok = malloc(sizeof(dog_t));
-	if (modok == NULL)
+	new_d = malloc(sizeof(dog_t));
+	if (new_d != NULL)
 	{
-		free(modok);
-	}
-
-	if (modok != NULL)
-	{
-		modok->name = strdup(name);
-		if (modok->name == NULL)
+		new_d->name = _strdup(name);
+		if (new_d->name == NULL)
 		{
-			free(modok);
+			free(new_d);
 			return (NULL);
 		}
-		modok->owner = strdup(owner);
-		if (modok->owner == NULL)
+		new_d->owner = _strdup(owner);
+		if (new_d->owner == NULL)
 		{
-			free(modok->name);
-			free(modok);
+			free(new_d->name);
+			free(new_d);
 			return (NULL);
 		}
-		modok->age = age;
+		new_d->age = age;
 	}
-	return (modok);
+	return (new_d);
+}
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: pointer to check
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len++;
+		s = s + 1;
+	}
+	return (len);
+}
+
+/**
+ * _strdup - copy of the string given as a parameter
+ * @str: string of char
+ *
+ * Description: The _strdup() function returns a pointer to a new string which
+ * is a duplicate of the string str. Memory for the new string is
+ * obtained with malloc, and can be freed with free.
+ * Returns NULL if str = NULL
+ *
+ * Return: a pointer to the duplicated string. It returns
+ * NULL if insufficient memory was available
+ */
+
+char *_strdup(char *str)
+{
+	char *res = NULL;
+	int i = 0;
+
+	if (str != NULL)
+		res = malloc(_strlen(str) + 1);
+	if (res != NULL)
+	{
+		for (i = 0; i <= _strlen(str); i++)
+			res[i] = str[i];
+	}
+	return (res);
 }
