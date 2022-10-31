bb35228f4124c38ad42464d8a77a727c6842f892
Washx23 <5731@holbertonstudents.com>
2022-10-31 09:41:15 -0700

    task 3

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,6 +11,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (cmp)
+	{
 	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
@@ -19,6 +21,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		{
 			return (i);
 		}
+	}
 	}
 	return (-1);
 }
