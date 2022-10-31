5faebaaa90df272a437e925520c3893b4bdc1b3e
Washx23 <5731@holbertonstudents.com>
2022-10-31 08:04:11 -0700

    1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -10,12 +10,14 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	if (action || array)
+	if (action)
 	{
-	for (i = 0; i < size; i++)
-	{
-		(action)(*array);
-		array++;
-	}
+		if(array)
+
+			for (i = 0; i < size; i++)
+		{
+			(action)(*array);
+			array++;
+		}
 	}
 }
