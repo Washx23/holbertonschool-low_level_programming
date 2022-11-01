6cff3a4db88917745f1abed3677b06608ead027d
Washx23 <5731@holbertonstudents.com>
2022-11-01 12:43:42 -0700

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,27 +1,27 @@
 #include "3-calc.h"
 
 /**
- * main - funcion
- * @argc: integer
- * @argv: intger
- * Return: always
+ * main - function
+ * @argc: int
+ * @argv: char
+ *
+ * Return: result of the operation
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int (*f)(int a, int b);
 
-	if (argc == 3)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	f  = get_op_func(argv[2]);
-	if ((*argv[2] == '/' && *argv[3] == 0) || (*argv[2] == '%' && *argv[3] == 0))
+	f = get_op_func(argv[2]);
+	if (*argv[3] == '0')
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
 	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
