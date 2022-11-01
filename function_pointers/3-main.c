2851c2197f30dae52e3845ccae40ba40c3993251
Washx23 <5731@holbertonstudents.com>
2022-11-01 12:31:49 -0700

    3-main.c

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,31 +8,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1;
-	int num2;
+	int (*f)(int a, int b);
 
-	if (argc)
+	if (argc != 4)
 	{
-		if (argc > 2)
-		{
-			printf("Error\n");
-			exit(99);
-		}
-		if (*argv[3] == '0')
-		{
-			printf("Error\n");
-			exit(100);
-		}
-		else if (strlen(argv[2]) > 1)
-		{
-			printf("Error\n");
-			exit(98);
-		}
-		{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
-		printf("%d\n", get_op_func(argv[2])(num1, num2));
-		}
+		printf("Error\n");
+		exit(98);
 	}
+	f  = get_op_func(argv[2]);
+	if ((*argv[2] == '/' && *argv[3] == 0) || (*argv[2] == '%' && *argv[3] == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
