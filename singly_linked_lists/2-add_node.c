53552b5a539650fa58be90d71b77e40b955ac7f8
Washx23 <5731@holbertonstudents.com>
2022-11-17 06:01:57 -0800

    first push of add_node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_node - function to add nodes in beginning
+ * @head: pointer to first node
+ * @str: node data
+ * Return: new 
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = NULL;
+	
+	new = malloc(sizeof(list_t));
+	if(!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
