b9f1fca5477ad84000054c5ce7816bdd7b4dc2f7
Washx23 <5731@holbertonstudents.com>
2023-06-30 20:25:14 -0700

    firts commit Almost a circle

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,12 @@
+"""document"""
+
+from models import base
+
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb__objects  +=1
+            self.id = Base.__nb_objects
